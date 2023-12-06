184271061bb5e2a35133a6b1b298d4395c837f28
Gabriel Romanini de Barros <gabriel.romanini.barros@gmail.com>
2023-12-06 17:16:41 -0500

    Finished Bishop logic

--- Bishop.java
+++ Bishop.java
@@ -1,27 +1,68 @@
-public class Bishop extends Piece{
+public class Bishop extends Piece {
 
-    public Bishop(int a, int b, boolean c){
+    public Bishop(int a, int b, boolean c) {
         super(a, b, c);
     }
 
-    boolean isEmpty(){
+    boolean isEmpty() {
         return false;
     }
 
-    public void refreshLegalMoves(Piece[][] board){
+    public void refreshLegalMoves(Piece[][] board) {
 
         resetLegalMoves();
 
         //Checks path up and to the right
-        for(int x = coordinates[0] + 1, y = coordinates[1] + 1; x > 0 && y > 0; x++, y++)
-            if( board[x][y].isEmpty())
+        for (int x = coordinates[0] + 1, y = coordinates[1] - 1; x < 8 && y > 0; x++, y--) {
+            if (board[x][y].isEmpty())
                 legalMoves[x][y] = 1;
-            else if(board[x][y].isWhite != isWhite ) {
+            else if (board[x][y].isWhite != isWhite) {
                 legalMoves[x][y] = 1;
                 break;
             }
             else
                 break;
+
+        }
+
+        //Checks path up and to the left
+        for (int x = coordinates[0] - 1, y = coordinates[1] - 1; x > 0 && y > 0; x--, y--) {
+            if (board[x][y].isEmpty())
+                legalMoves[x][y] = 1;
+            else if (board[x][y].isWhite != isWhite) {
+                legalMoves[x][y] = 1;
+                break;
+            }
+            else
+                break;
+
+        }
+
+        //Checks path down and to the right
+        for (int x = coordinates[0] + 1, y = coordinates[1] + 1; x < 8 && y < 8; x++, y++) {
+            if (board[x][y].isEmpty())
+                legalMoves[x][y] = 1;
+            else if (board[x][y].isWhite != isWhite) {
+                legalMoves[x][y] = 1;
+                break;
+            }
+            else
+                break;
+
+        }
+
+        //Checks path down and to the left
+        for (int x = coordinates[0] - 1, y = coordinates[1] + 1; x > 0 && y < 8; x--, y++) {
+            if (board[x][y].isEmpty())
+                legalMoves[x][y] = 1;
+            else if (board[x][y].isWhite != isWhite) {
+                legalMoves[x][y] = 1;
+                break;
+            }
+            else
+                break;
+
+        }
 
     }
 

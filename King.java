08bef35a264b2070935e7f9a70ae6f1fd95870a9
Gabriel Romanini de Barros <gabriel.romanini.barros@gmail.com>
2023-12-06 23:41:23 -0500

    Finished checkmate function

--- King.java
+++ King.java
@@ -1,5 +1,15 @@
-public class King {
+public class King extends Piece{
 
+    public boolean isChecked(Piece[][] board){
+
+        for(int i = 0; i < 8; i++)
+            for(int j = 0; j < 8; j++)
+                if(!board[i][j].isEmpty() && board[i][j].getLegalMoves( coordinates[0], coordinates[1]) == 1 )
+                    return true;
+
+        return false;
+
+    }
 
 
 }
